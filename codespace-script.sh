001ca176ad34f5e4260c8874cb6d674647a3a64b
Saju <129554737+sajusal@users.noreply.github.com>
2025-01-31 17:24:35 -0500

    Update codespace-script.sh

--- codespace-script.sh
+++ codespace-script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -eux
+
 curl -sL https://containerlab.dev/setup | sudo -E bash -s "all"
 
 sudo bash -c "$(curl -sL https://get-gribic.kmrd.dev)"
@@ -7,14 +7,6 @@ sudo bash -c "$(curl -sL https://get-gribic.kmrd.dev)"
 sudo bash -c "$(curl -sL https://get-gnmic.openconfig.net)"
 
 sudo bash -c "$(curl -sL https://get-gnoic.kmrd.dev)"
-
-wget https://go.dev/dl/go1.22.11.linux-amd64.tar.gz
-
-sudo rm -rf /usr/local/go
-
-sudo tar -C /usr/local -xzf go1.22.11.linux-amd64.tar.gz
-
-export PATH=$PATH:/usr/local/go/bin
 
 git clone https://github.com/karimra/gnsic.git
 
