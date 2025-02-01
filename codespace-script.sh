258479738d29a9b2615d4b147383f9632906caab
Saju <129554737+sajusal@users.noreply.github.com>
2025-02-01 18:40:28 -0500

    Update codespace-script.sh

--- codespace-script.sh
+++ codespace-script.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
-curl -sL https://containerlab.dev/setup | sudo -E bash -s "all"
-
 sudo bash -c "$(curl -sL https://get-gribic.kmrd.dev)"
-
-sudo bash -c "$(curl -sL https://get-gnmic.openconfig.net)"
-
-sudo bash -c "$(curl -sL https://get-gnoic.kmrd.dev)"
 
 git clone https://github.com/karimra/gnsic.git
 
